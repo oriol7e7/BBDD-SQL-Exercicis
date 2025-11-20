93f95b5f0feb5255781e4b80894bd7bb48f993e6
oriol7e7 <oriol.plazas.7e7@itb.cat>
2025-11-20 13:00:50 +0100

    add alter tables

--- RA2_EA4_PlazasOriol.sql
+++ RA2_EA4_PlazasOriol.sql
@@ -61,3 +61,36 @@ CREATE TABLE IF NOT EXISTS casaparticular(
     metres_c NUMERIC(4),
     dni_cp VARCHAR(12)
 );
+
+--Alter table per afegir constraint
+
+ALTER TABLE zonaurbana
+ADD CONSTRAINT PK_zonaurbana PRIMARY KEY (nom_zona),
+ADD CONSTRAINT CK_nomZonaMajus CHECK (nom_zona = UPPER(nom_zona));
+
+ALTER TABLE vivenda
+ADD CONSTRAINT PK_vivenda PRIMARY KEY (carrer, nombre),
+ADD CONSTRAINT FK_vivenda_zonaurbana FOREIGN KEY (nom_zona) REFERENCES zonaurbana(nom_zona),
+ADD CONSTRAINT CK_carrerPrimeraMajus CHECK (carrer = INITCAP(carrer)),
+ADD CONSTRAINT CK_nombreMajor0 CHECK (nombre>0),
+ADD CONSTRAINT CK_tipus_vivendaIn CHECK (tipus_vivenda IN ('C', 'B'));
+
+--Afegir default, no es pot en la mateixa comanda (block) que les constraints
+ALTER TABLE vivenda
+ALTER COLUMN codi_postal SET DEFAULT '00001';
+
+ALTER TABLE persona
+ADD CONSTRAINT PK_persona PRIMARY KEY (dni),
+ADD CONSTRAINT FK_persona_vivenda_carrer_nombre FOREIGN KEY (carrer, nombre) REFERENCES vivenda (carrer, nombre),
+ADD CONSTRAINT FK_persona_dni_c FOREIGN KEY (dni_c) REFERENCES persona(dni)
+
+ALTER TABLE bloccases 
+ADD CONSTRAINT PK_bloccases PRIMARY KEY (carrer, nombre),
+ADD CONSTRAINT FK_bloccases_vivenda_carrer_nombre FOREIGN KEY (carrer, nombre) REFERENCES vivenda (carrer, nombre) ON DELETE CASCADE,
+ADD CONSTRAINT CK_metresbMajor0 CHECK (metres_b>0);
+
+ALTER TABLE casaparticular
+ADD CONSTRAINT PK_casaparticular PRIMARY KEY (carrer, nombre),
+ADD CONSTRAINT FK_casaparticular_persona_dnicp FOREIGN KEY dni_cp REFERENCES persona(dni),
+ADD CONSTRAINT FK_casaparticular_vivenda_carrer_nombre FOREIGN KEY (carrer, nombre) REFERENCES vivenda (carrer, nombre) ON DELETE CASCADE,
+ADD CONSTRAINT CK_metrescMajor0 CHECK (metres_c>0);
