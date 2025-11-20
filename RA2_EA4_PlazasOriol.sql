6beeb49f173fd91b5dbe7797ff530e46ca29a028
oriol7e7 <oriol.plazas.7e7@itb.cat>
2025-11-20 13:01:04 +0100

    add alter tables

--- RA2_EA4_PlazasOriol.sql
+++ RA2_EA4_PlazasOriol.sql
@@ -62,6 +62,15 @@ CREATE TABLE IF NOT EXISTS casaparticular(
     dni_cp VARCHAR(12)
 );
 
+CREATE TABLE IF NOT EXISTS habitapis(
+    dni VARCHAR(12) NOT NULL
+    carrer VARCHAR(80) NOT NULL,
+    nombre NUMERIC(4) NOT NULL,
+    escala VARCHAR(1) NOT NULL,
+    planta NUMERIC(2) NOT NULL,
+    porta VARCHAR(2) NOT NULL,
+);
+
 --Alter table per afegir constraint
 
 ALTER TABLE zonaurbana
@@ -81,16 +90,27 @@ ALTER COLUMN codi_postal SET DEFAULT '00001';
 
 ALTER TABLE persona
 ADD CONSTRAINT PK_persona PRIMARY KEY (dni),
-ADD CONSTRAINT FK_persona_vivenda_carrer_nombre FOREIGN KEY (carrer, nombre) REFERENCES vivenda (carrer, nombre),
+ADD CONSTRAINT FK_persona_vivenda_carrer_nombre FOREIGN KEY (carrer, nombre) REFERENCES vivenda(carrer, nombre),
 ADD CONSTRAINT FK_persona_dni_c FOREIGN KEY (dni_c) REFERENCES persona(dni)
 
 ALTER TABLE bloccases 
 ADD CONSTRAINT PK_bloccases PRIMARY KEY (carrer, nombre),
-ADD CONSTRAINT FK_bloccases_vivenda_carrer_nombre FOREIGN KEY (carrer, nombre) REFERENCES vivenda (carrer, nombre) ON DELETE CASCADE,
+ADD CONSTRAINT FK_bloccases_vivenda_carrer_nombre FOREIGN KEY (carrer, nombre) REFERENCES vivenda(carrer, nombre) ON DELETE CASCADE,
 ADD CONSTRAINT CK_metresbMajor0 CHECK (metres_b>0);
 
 ALTER TABLE casaparticular
 ADD CONSTRAINT PK_casaparticular PRIMARY KEY (carrer, nombre),
 ADD CONSTRAINT FK_casaparticular_persona_dnicp FOREIGN KEY dni_cp REFERENCES persona(dni),
-ADD CONSTRAINT FK_casaparticular_vivenda_carrer_nombre FOREIGN KEY (carrer, nombre) REFERENCES vivenda (carrer, nombre) ON DELETE CASCADE,
+ADD CONSTRAINT FK_casaparticular_vivenda_carrer_nombre FOREIGN KEY (carrer, nombre) REFERENCES vivenda(carrer, nombre) ON DELETE CASCADE,
 ADD CONSTRAINT CK_metrescMajor0 CHECK (metres_c>0);
+
+ALTER TABLE pis 
+ADD CONSTRAINT PK_pis PRIMARY KEY (carrer, nombre, escala, planta, porta),
+ADD CONSTRAINT FK_casaparticular_bloccases_carrer_nombre FOREIGN KEY (carrer, nombre) REFERENCES bloccases(carrer, nombre) ON DELETE CASCADE,
+ADD CONSTRAINT FK_pis_persona_dnip FOREIGN KEY dni_p REFERENCES persona(dni) ON DELETE CASCADE,
+ADD CONSTRAINT CK_metres_pMajor0 CHECK (metres_p>0);
+
+ALTER TABLE habitapis
+ADD CONSTRAINT PK_habitapis PRIMARY KEY (dni),
+ADD CONSTRAINT FK_habitapis_persona_dni FOREIGN KEY dni REFERENCES persona(dni),
+ADD CONSTRAINT FK_habitapis_pis FOREIGN KEY (carrer, nombre, escala, planta, porta) REFERENCES pis(carrer, nombre, escala, planta, porta) ON DELETE CASCADE;
