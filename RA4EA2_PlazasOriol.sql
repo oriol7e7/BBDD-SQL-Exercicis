80850693bf59e55cdfa44debf9cfada78d46bd33
oriol7e7 <oriol.plazas.7e7@itb.cat>
2025-12-05 09:05:48 +0100

    comlpete ra4ea2

--- RA4EA2_PlazasOriol.sql
+++ RA4EA2_PlazasOriol.sql
@@ -107,3 +107,163 @@ VALUES (
     '06/07/1978',
     2
 );
+
+----
+BEGIN
+
+INSERT INTO FITXA VALUES(
+    7868544, 
+    'JONÁS', 
+    'ALMENDROS RODRÍGUEZ',
+    'FEDERICO PUERTAS, 3',
+    '915478947',
+    'MADRID',
+    '01/01/1987',
+    3
+);
+INSERT INTO FITXA VALUES(
+    8324216, 
+    'PEDRO', 
+    'MARTÍN HIGUERO',
+    'VIRGEN DEL CERRO, 154',
+    '961522344',
+    'SORIA',
+    '29/04/1978',
+    5
+);
+
+---
+
+INSERT INTO FITXA VALUES(
+    14948992, 
+    'SANDRA', 
+    'MARTÍN GONZÁLEZ',
+    'PABLO NERUDA, 15',
+    '916581515',
+    'MADRID',
+    '05/05/1970',
+    6
+);
+
+---
+
+INSERT INTO FITXA VALUES(
+    15756214, 
+    'MIGUEL', 
+    'CAMARGO ROMÁN',
+    'ARMADORES, 1',
+    '949488588',
+    NULL,
+    '12/12/1985',
+    7
+);
+
+COMMIT intA;
+
+---
+
+INSERT INTO FITXA VALUES(
+    21158230, 
+    'SERGIO', 
+    'ALFARO IBIRICU',
+    'AVENIDA DEL EJERCITO, 76',
+    '934895855',
+    'BCN',
+    '11/11/1987',
+    8
+);
+
+INSERT INTO FITXA VALUES(
+    34225234, 
+    'ALEJANDRO', 
+    'ALCOCER JARABO',
+    'LEONOR DE CORTINAS, 7',
+    '935321211',
+    'MADRID',
+    '05/05/1970',
+    9
+);
+
+COMMIT intB;
+
+---
+
+INSERT INTO FITXA VALUES(
+    38624852, 
+    'ALVARO', 
+    'RAMÍREZ AUDIGE',
+    'FUENCARRAL, 33',
+    '912451168',
+    'MADRID',
+    '10/09/1976',
+    10
+);
+
+COMMIT intC;
+
+---
+
+INSERT INTO FITXA VALUES(
+    45824852, 
+    'ROCÍO', 
+    'PÉREZ DEL OLMO',
+    'CERVANTES, 22',
+    '912332138',
+    'MADRID',
+    '06/12/1987',
+    11
+);
+
+INSERT INTO FITXA VALUES(
+    48488588, 
+    'JESÚS', 
+    'BOBADILLA SANCHO',
+    'GAZTAMBIQUE, 32',
+    '913141111',
+    'MADRID',
+    '05/05/1970',
+    13
+);
+
+COMMIT intD;
+
+---
+
+--Esborrar registre amb DNI: 45824852.
+
+DELETE FROM FIXTA WHERE dni = 45824852;
+
+COMMIT intE;
+
+SELECT * FROM FITXA;
+
+
+-- Modificar registre
+UPDATE FITXA SET EQUIP = 11 WHERE DNI = 48488588;
+
+COMMIT intF;
+
+ROLLBACK intE;
+SELECT * FROM FITXA;
+
+ROLLBACK intD;
+SELECT * FROM FITXA;
+
+-- Modificar registre
+UPDATE FITXA SET EQUIP = 11 WHERE DNI = 38624852.;
+SELECT * FROM FITXA;
+
+
+--
+INSERT INTO FITXA VALUES(
+    98987765, 
+    'PEDRO', 
+    'RUIZ RUIZ',
+    'SOL, 43',
+    '91-656-43-32',
+    'MADRID',
+    '10/09/1976',
+    12
+);
+
+SELECT * FROM FITXA;
