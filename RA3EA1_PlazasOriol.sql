5a7abc908acea59a546d0bef9454a046a89a2351
Oriol Plazas <oriolplazas2@gmail.com>
2026-01-16 12:48:09 +0100

    Add SQL queries for employee data analysis

--- RA3EA1_PlazasOriol.sql
+++ RA3EA1_PlazasOriol.sql
@@ -29,3 +29,36 @@ ORDER BY last_name;
 
 
 --ex6
+SELECT last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN DATE '1998-01-01' AND DATE '1998-12-31';
+
+
+--ex7
+SELECT last_name, job_id
+FROM employees
+WHERE manager_id IS NULL;
+
+--ex8
+SELECT last_name, salary, commission_pct
+FROM employees
+WHERE commission_pct IS NOT NULL
+ORDER BY salary DESC, commission_pct DESC;
+
+
+--ex9
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '__a%';
+
+
+--ex10
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%a%' and last_name LIKE '%e%';
+
+
+--ex11
+SELECT last_name, job_id, salary
+FROM employees
+WHERE (job_id = 'AC_ACCOUNT' or job_id = 'AD_ASST') AND salary NOT IN (2500, 3500, 7000);
