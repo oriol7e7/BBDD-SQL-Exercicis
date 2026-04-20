3c3c849a85075d1bcae4c79860d0a841aa0bc410
Oriol Plazas Leon <oriol.plazas.7e7@itb.cat>
2026-04-20 11:00:22 +0000

    add ra5ea2

---
+++ RA5-PL-SQL/RA5EA2_PlazasOriol.sql
@@ -0,0 +1,90 @@
+--EX1
+do $$
+    declare
+        num1 REAL;
+        num2 REAL;
+        result Numeric(10,2);
+    begin
+        num1 := :num1;
+        num2 := :num2;
+        if num1 < 0 OR num2 <0 then
+            raise notice 'Han de ser numeros positius';
+        else
+            result := num1/num2 + num2;
+            raise notice '%', result;
+        end if;
+    end;
+$$
+
+--EX2
+do $$
+    declare
+        num1 REAL;
+        num2 REAL;
+        result Numeric(10,2);
+    begin
+        num1 := :num1;
+        num2 := :num2;
+        if num1 < 0 OR num2 <0 then
+            raise notice 'Han de ser numeros positius';
+        elsif num2>num1 then
+            raise notice 'Error! el primer número ha de ser més gran que el segon.';
+        else
+            result := num1/num2 + num2;
+            raise notice '%', result;
+        end if;
+    end;
+$$
+
+
+--EX3
+do $$
+declare
+    age INTEGER;
+begin
+    age := :age;
+    case
+        when age between 0 and 17 then
+            raise notice 'Ets menor';
+        when age between 18 and 40 then
+            raise notice 'Ja ets major de edat';
+        when age > 40 then
+            raise notice 'Ja ets força gran';
+        when age < 0 then
+            raise notice 'La edat no pot ser negativa';
+    end case;
+end;
+$$
+
+
+--EX4
+do $$
+declare
+    a real;
+    b real;
+    operation integer;
+begin
+    a := :a;
+    b := :b;
+    raise notice 'opció 1 SUMAR, opció 2 RESTAR, opció 3 MULTIPLICAR, opció 4 DIVIDIR .';
+    operation := :operation;
+    case operation
+        when 1 then
+            raise notice 'has escollit suma';
+            RAISE NOTICE '% + % = %', a, b, a + b;
+        when 2 then
+            raise notice 'has escollit resta';
+            RAISE NOTICE '% - % = %', a, b, a - b;
+        when 3 then
+            raise notice 'has escollit multiplicacio';
+            RAISE NOTICE '% * % = %', a, b, a * b;
+        when 4 then
+            raise notice 'has escollit divisio';
+            RAISE NOTICE '% / % = %', a, b, a / b;
+        else
+            RAISE NOTICE 'Opcio no valida';
+    end case;
+end;
+$$
+
+
