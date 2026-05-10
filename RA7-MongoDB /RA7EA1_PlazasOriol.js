a2b8b96a027b0f6e7f3215894436004e23d1acf2
Oriol Plazas Leon <oriol.plazas.7e7@itb.cat>
2026-05-10 15:56:50 +0000

    add ra7

---
+++ RA7-MongoDB /RA7EA1_PlazasOriol.js
@@ -0,0 +1,9 @@
+//EX 1
+/*
+mongoimport --db itb --collection people --drop --type json --file C:\Users\Oriol\Descargas\people.json --jsonArray
+mongoimport --db itb --drop --type csv --headerline --file C:\Users\Oriol\Downloads\students.csv
+mongoimport --db itb --collection products --drop --file C:\Users\Oriol\Downloads\products.json
+*/
+
+//EX2
+use catalogo
